e0bdd805afe2df28c5754426bcb7185e99159a1b
Blake Mitchell <Blake.Mitchell@clarabridge.com>
2015-04-23 17:37:05 -0700

    move tar restore state into it's own file

--- mongorestore/mongorestore.go
+++ mongorestore/mongorestore.go
@@ -279,7 +279,8 @@ func (restore *MongoRestore) RestoreFileDump() error {
 	return nil
 }
 
-// serially build and restore intents from a tar archive
+// serially build and restore intents from a tar archive, there is no random
+// access, so it is not possilbe to use an intents.Manager
 func (restore *MongoRestore) RestoreTarDump() error {
 	log.Log(log.Always, "restoring from a tar archive, unable to detect file errors before restore begins")
 
@@ -394,74 +395,4 @@ func (restore *MongoRestore) RestoreTarDump() error {
 	}
 	// rstore indexes for last collection
 	return state.ChangeCollection("")
-}
-
-type TarRestoreState struct {
-	PastDbs           []string
-	CurrentDb         string
-	SingleDb          bool
-	PastCollections   []string
-	CurrentCollection string
-	SingleCollection  bool
-	UsesMetadataFiles bool
-	RestoredMetadata  bool
-	RestoredBSON      bool
-	CollectionExists  bool
-	MetadataIndexes   []IndexDocument
-	SystemIndexes     map[string][]IndexDocument
-	Intent            *intents.Intent
-	restore           *MongoRestore
-}
-
-func (restore *MongoRestore) NewTarRestoreState() *TarRestoreState {
-	return &TarRestoreState{
-		PastDbs:           make([]string, 0, 10),
-		CurrentDb:         restore.ToolOptions.DB,
-		SingleDb:          restore.ToolOptions.DB != "",
-		PastCollections:   make([]string, 0, 10),
-		CurrentCollection: restore.ToolOptions.Collection,
-		SingleCollection:  restore.ToolOptions.Collection != "",
-		UsesMetadataFiles: false,
-		RestoredMetadata:  false,
-		RestoredBSON:      false,
-		CollectionExists:  false,
-		MetadataIndexes:   nil,
-		SystemIndexes:     nil,
-		Intent:            nil,
-		restore:           restore,
-	}
-}
-
-func (state *TarRestoreState) ChangeDatabase(db string) error {
-	if state.CurrentDb != "" {
-		state.PastDbs = append(state.PastDbs, state.CurrentDb)
-	}
-	state.CurrentDb = db
-	state.PastCollections = make([]string, 0, 10)
-	state.SystemIndexes = nil
-	return state.ChangeCollection("")
-}
-
-func (state *TarRestoreState) ChangeCollection(collection string) error {
-	if state.CurrentCollection != "" {
-		state.PastCollections = append(state.PastCollections, state.CurrentCollection)
-	}
-	state.CurrentCollection = collection
-	state.UsesMetadataFiles = false
-	if state.RestoredMetadata {
-		err := state.restore.RestoreCollectionIndexes(state.Intent, state.MetadataIndexes)
-		if err != nil {
-			return err
-		}
-	} else if state.SystemIndexes != nil && state.SystemIndexes[state.Intent.C] != nil {
-		err := state.restore.RestoreCollectionIndexes(state.Intent, state.SystemIndexes[state.Intent.C])
-		if err != nil {
-			return err
-		}
-	}
-	state.RestoredMetadata = false
-	state.RestoredBSON = false
-	state.CollectionExists = false
-	state.MetadataIndexes = nil
-	return nil
 }
